6972571842400709d502c52eaff2cac8b7a64a84
soydan-ridvan <rsoydan@breaker19.app>
2025-10-16 14:39:05 +0300

    chore(tests): add demo todo spec under tests and update package files

--- tests/e13_bootstrapDropdown.spec.js
+++ tests/e13_bootstrapDropdown.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} = require ('@playwright/test')
 
-test ('@job Bootstrap Dropdown', async ({page})=>{
+test ('Bootstrap Dropdown', async ({page})=>{
 
     await page.goto('https://www.jquery-az.com/boots/demo.php?ex=63.0_2')
 
